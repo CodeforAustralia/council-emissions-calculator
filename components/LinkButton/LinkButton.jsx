f999fed0ac36f3a9496f2a81c755fdd715a5d7fe
Kris Lubiniecki <klubiniecki@gmail.com>
2021-07-23 05:57:39 +1000

    Update LinkButton.jsx

--- components/LinkButton/LinkButton.jsx
+++ components/LinkButton/LinkButton.jsx
@@ -4,17 +4,15 @@ import { Button } from "@chakra-ui/react";
 export default function LinkButton({ children, href, onClick, disabled }) {
   return (
     <Link href={href} passHref>
-      <a>
-        <Button
-          mt={8}
-          px="12"
-          colorScheme="blue"
-          disabled={disabled}
-          onClick={onClick}
-        >
-          {children}
-        </Button>
-      </a>
+      <Button
+        mt={8}
+        px="12"
+        colorScheme="blue"
+        disabled={disabled}
+        onClick={onClick}
+      >
+        {children}
+      </Button>
     </Link>
   );
 }
