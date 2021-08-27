4cf325106b4998ec53a51738bba56ebe8c0a4864
meng <loh.yumeng@gmail.com>
2021-08-27 11:20:26 +1000

    add outline-variant option to LinkButton

--- components/LinkButton/LinkButton.jsx
+++ components/LinkButton/LinkButton.jsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import { Button } from "@chakra-ui/react";
 
-export default function LinkButton({ children, href, onClick, disabled }) {
+export default function LinkButton({ children, href, onClick, disabled, outline }) {
   return (
     <Link href={href} passHref>
       <Button
         mt={8}
         px="12"
         colorScheme="blue"
+        variant={ outline ? "outline" : "solid" }
         disabled={disabled}
         onClick={onClick}
       >
