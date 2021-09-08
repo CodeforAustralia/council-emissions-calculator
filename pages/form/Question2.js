6472950fae6a055f752c49b9ecdde4584c1fefeb
meng <loh.yumeng@gmail.com>
2021-09-08 10:47:16 +1000

    fix Continue btn disabling; fix question dependence on previous answers

--- pages/form/Question2.js
+++ pages/form/Question2.js
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useState } from "react";
 import {
   Box,
   Grid,
@@ -14,27 +14,46 @@ import { BackButton, ContinueButton } from "../../components/LinkButton/LinkButt
 
 export default function Question2() {
   const { answers, setAnswers } = useForm();
+  const [ selectedMode, setSelectedMode ] = useState("");
 
-  const select = useRef(null);
+  const saveAnswers = () => setAnswers(prev => ({ ...prev, mainModeOfTransport: selectedMode }));
 
-  const saveAnswers = () => setAnswers(prev => ({ ...prev, mainModeOfTransport: select.current.value }));
+  // check if user is working from home (i.e., never travelling to the office)
+  let isWFH = answers.week.every(day => day !== 'office');
 
   return (
     <Layout>
       <Q2Progress />
-      <Box mt={6}>
-        <Heading as="h1" size="md">How do you travel to work in an average week?</Heading>
-        <Text my={5}>Select the main way you travel to work.</Text>
-        <Text my={5}>For example, if you usually drive 2km to the train and then catch the train for 15km, choose train as your way of travel.</Text>
-        <Text my={5}>If you currently work from home, we will use this information to calculate the emissions you save by working at home.</Text>
-        <Select ref={select} placeholder="Select travel method" alignSelf="start" w="50%">
-          {modesOfTransport.map(mode => <option key={mode} value={mode}>{mode}</option>)}
-        </Select>
-      </Box>
-      <Grid templateColumns="repeat(2, 1fr)" gap={4}>
-        <BackButton href="/form/Question1" onClick={saveAnswers} />
-        <ContinueButton href="/form/Question3" onClick={saveAnswers} />
-      </Grid>
+      {
+        isWFH
+          ?
+          <>
+            <Heading as="h1" size="md" mt={6}>Looks like you are working from home!</Heading>
+            <Text my={5}>Move to the next question.</Text>
+            <ContinueButton href="/form/Question3" />
+          </>
+          :
+          <>
+            <Box mt={6}>
+              <Heading as="h1" size="md">How do you travel to work in an average week?</Heading>
+              <Text my={5}>Select the main way you travel to work.</Text>
+              <Text my={5}>For example, if you usually drive 2km to the train and then catch the train for 15km, choose train as your way of travel.</Text>
+              <Text my={5}>If you currently work from home, we will use this information to calculate the emissions you save by working at home.</Text>
+              <Select
+                placeholder="Select travel method"
+                alignSelf="start"
+                w="50%"
+                onChange={(e) => setSelectedMode(e.target.value)}
+              >
+                {modesOfTransport.map(mode => <option key={mode} value={mode}>{mode}</option>)}
+              </Select>
+            </Box>
+            <Grid templateColumns="repeat(2, 1fr)" gap={4}>
+              <BackButton href="/form/Question1" onClick={saveAnswers} />
+              <ContinueButton href="/form/Question3" onClick={saveAnswers} disabled={!selectedMode} />
+            </Grid>
+          </>
+      }
     </Layout>
   );
 }
