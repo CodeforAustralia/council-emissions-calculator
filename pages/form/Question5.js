851f08e34256edf412d5e5e46818768dcad967d4
Bhagyashree <bhagya099@gmail.com>
2021-09-14 15:52:37 +1000

    create both button

--- pages/form/Question5.js
+++ pages/form/Question5.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Heading, Text, Select } from '@chakra-ui/react';
+import { Heading, Text, Select, Stack, Grid } from '@chakra-ui/react';
 import Layout from '../../components/Layout/Layout';
 import useForm from '../../components/FormProvider';
 import { departments } from '../../utils/constants';
@@ -7,6 +7,7 @@ import LinkButton from '../../components/LinkButton/LinkButton';
 import {
   BackButton,
   ContinueButton,
+  SubmitButton,
 } from '../../components/LinkButton/LinkButton';
 import capitalize from '../../utils/capitalize';
 import Q5Progress from '../../public/images/progress-bar/q5-progress-bar.svg';
@@ -41,21 +42,21 @@ export default function Question5() {
           </option>
         ))}
       </Select>
-      <BackButton href='/form/Question4' onClick={saveAnswers} />
-      <LinkButton
-        href='/results'
-        disabled={!department}
-        onClick={() =>
-          setAnswers((prev) => {
-            const response = { ...prev, department };
-            //console.log(`form 5 updates: ${JSON.stringify(response)}`);
-            sendFormResponse(response);
-            return response;
-          })
-        }
-      >
-        Submit
-      </LinkButton>
+      <Grid templateColumns='repeat(2, 1fr)' gap={4}>
+        <BackButton href='/form/Question4' onClick={saveAnswers} />
+        <SubmitButton
+          href='/results'
+          disabled={!department}
+          onClick={() =>
+            setAnswers((prev) => {
+              const response = { ...prev, department };
+              //console.log(`form 5 updates: ${JSON.stringify(response)}`);
+              sendFormResponse(response);
+              return response;
+            })
+          }
+        />
+      </Grid>
     </Layout>
   );
 }
