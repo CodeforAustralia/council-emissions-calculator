f9df7dd31e7e06cd21b1154762309a7acd8cd4b2
oonamcmanus <47853537+oonamcmanus@users.noreply.github.com>
2021-09-22 16:35:26 +1000

    Further updating 'how to run this script'
    I added some more specific instructions on running the script, and replaced calling the whole "tidyverse" library with instead just calling "ggplot2" (the only part of tidyverse needed for the revised script).

--- scripts/make_dummy_json.R
+++ scripts/make_dummy_json.R
@@ -1,18 +1,29 @@
 #This script creates "dummy data" for the Civic Makers carbon calculator project
 
-#HOW TO RUN THIS SCRIPT: 
-# 1) Install R from https://cran.ms.unimelb.edu.au/
-# 2) Uncomment the "install.packages" rows below if you don't already have these packages installed
-# 3) From Command line (set to the directory where this script is saved), run the command ` Rscript make_dummy_json.R ` 
-# 4) This should produce two outputs: a .png (?) that shows 
+#HOW TO RUN THIS SCRIPT:
+#1a) The easiest way, if possible, is to install the free desktop RStudio IDE from https://www.rstudio.com/products/rstudio/download/  The IDE installs R for you, and it makes it pretty painless to install packages (it works out the dependencies for you). It also has a Viewer that shows you the visualisations, rather than downloading them as a separate file.
+
+#1b) OR - If you DON'T want to install RStudio - then Install R from https://cran.ms.unimelb.edu.au/
+
+# 2) Uncomment the "install.packages" rows of code at the top of the script below if you don't already have these packages installed
+
+# 3a) If you're using RStudio, you can select the lines of the script that you want to run and hit "Run" (selecting everything is fine, but it may be useful to run line-by-line if you want to watch the interim results)
+
+# 3b) If you're not using RStudio, then from Command line (set to the directory where this script is saved), run the command ` Rscript make_dummy_json.R ` 
+
+# 4) This should produce two outputs: a json file called 'jsonmodes.json' containing sample transport data in json form, and the image file with the sample ggridges chart
+
+
+#Packages to install: uncomment these lines to install the packages!
 
 # install.packages("ggplot2")
 # install.packages("jsonlite")
 # install.packages("ggridges")
 
-library(tidyverse)  #packages for manipulating data
+
 library(jsonlite) #package for converting dataframe into json
-library(ggridges) #package for making a ridgeline chart, so we can preview what the dataset should look like as a ridgeline chart in Highcharts
+library(ggplot2)  #package for creating a chart
+library(ggridges) #package for making a ggplot2 chart into a ridgeline chart, so we can preview what the dataset should look like as a ridgeline chart in Highcharts
 
 
 #This makes a vector of N numbers (N being the first number in the brackets!), randomly sampled from the normal distribution using the mean and standard deviation specified. I've just tried to guess the mean & sd for each of the transport types, and assumed there would be more responses for car/train/bike/walk than for motorbike/taxi... very broad guesses, may need to be tweaked!!
@@ -35,7 +46,7 @@ modes <- rbind(
   data.frame(mode = "Motorbike", distance = Motorbike),
   data.frame(mode = "Bus", distance = Bus),
   data.frame(mode = "Taxi", distance = Taxi)
-  )
+)
 
 
 #This makes a plot out of the dataset that should closely resemble what we're making in highcharts, just to see what the shape should be!
