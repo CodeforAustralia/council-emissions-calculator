246d81022fe32a534905a0a7fc9ec5b0d9725115
hqtan <hanqing.tan@gmail.com>
2021-09-26 22:03:19 +1000

    add `TripsChart` react component, and `getTripCounts` function in `utils/dbApi.js`: - `getTripCounts()` `fetch`es trip count data from google sheets via   `api/trips` API route
    in `pages/results.js`:
    - `TripsChart` component contains the 'Top 3 Trip Counts by Transport
      Mode' bar chart; component takes in trip count data via its props
    - `getServerSideProps()` is used to asynchronously retrieve trip count
      data (using `getTripCounts()`). See this following docs for more
    details about how `getServerSideProps()` works in next.js:
      - https://nextjs.org/docs/basic-features/data-fetching#getserversideprops-server-side-rendering
    
    in `.env.development` and `.env.production`:
    - create `NEXT_PUBLIC_HOST` environment variable to declare the app's
      hostname in dev and prod environment

--- pages/results.js
+++ pages/results.js
@@ -5,16 +5,76 @@ import Layout from "../components/Layout/Layout";
 // import calculateEmissions from "../utils/calculateEmissions";
 import capitalize from "../utils/capitalize";
 import { daysOfWeek } from "../utils/constants";
-import { getFormResponses } from "../utils/dbApi";
+import { getTripCounts } from "../utils/dbApi";
+import Highcharts from 'highcharts';
+import HighchartsExporting from 'highcharts/modules/exporting';
+import HighchartsReact from 'highcharts-react-official';
 
-export default function Results() {
+if (typeof Highcharts === 'object') {
+  HighchartsExporting(Highcharts)
+}
+
+export async function getServerSideProps() {
+  const tripjson = await getTripCounts();
+
+  // console.log(`is tripjson ready?: ${ !!tripjson }`);
+  // console.log(`tripjson: ${ JSON.stringify(tripjson) }`);
+
+  return {
+    props: {
+      data: tripjson
+    }
+  };
+}
+
+const TripsChart = ({ data }) => {
+  // console.log(`data: ${ JSON.stringify(data) }`);
+
+  if (!data) return <div>I got nothing...</div>
+
+  let topTransportModes = data.map(t => t[0]);
+  let tripCounts = data.map(t => Number(t[1]));
+  // console.log(`topTransportModes: ${ JSON.stringify(topTransportModes) }`);
+  // console.log(`tripCounts: ${ JSON.stringify(tripCounts) }`);
+
+  const hichartsOpts = {
+    title: {
+      text: 'Trip Count by Transport Mode'
+    },
+    chart: {
+      type: 'bar'
+    },
+    xAxis: {
+      // categories: ['Walking', 'Train/tram', 'E-scooter']
+      categories: topTransportModes
+    },
+    series: [{
+      name: 'Trip Count',
+      data: tripCounts
+      // data: [37, 30, 28]
+    }]
+  }
+
+  return (
+    <HighchartsReact
+      highcharts={Highcharts}
+      options={hichartsOpts}
+    />
+  );
+}
+
+export default function Results({ data }) {
   const { answers } = useForm();
   const { km, mainTransportMode, department, incentive } = answers;
   // const { form_responses } = getFormResponses();
+  // const results = calculateEmissions(km, transportModes);
+
+  // console.log(`fetched data: ${ JSON.stringify(data) }`);
 
   return (
     <Layout>
       <Heading>Your results:</Heading>
+      <TripsChart data={data} />
       <Text>
         <br />
         <b>Department:</b> {capitalize(department)}
