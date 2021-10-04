149a633ae78dfb8d581fbf0974d74e530eead792
hqtan <hanqing.tan@gmail.com>
2021-10-04 14:26:28 +1100

    add @Amutha37's changes for download button from PR #31 (https://github.com/CodeforAustralia/council-emissions-calculator-spike/pull/31)

--- pages/results.js
+++ pages/results.js
@@ -1,20 +1,26 @@
-import { Heading, Text } from "@chakra-ui/react";
+import { Heading, Text, Button } from "@chakra-ui/react";
 import React from "react";
 import useForm from "../components/FormProvider";
 import Layout from "../components/Layout/Layout";
-// import calculateEmissions from "../utils/calculateEmissions";
+
 import capitalize from "../utils/capitalize";
 import { daysOfWeek } from "../utils/constants";
 import { getFormResponses } from "../utils/dbApi";
+import { FiDownload } from "react-icons/fi";
 
 export default function Results() {
   const { answers } = useForm();
   const { km, mainTransportMode, department, incentive } = answers;
-  // const { form_responses } = getFormResponses();
 
   return (
     <Layout>
+      {/* Button for download */}
+      <Button className="resultbtn" colorScheme="blue">
+        Download as PDF or CSV {""}
+        <FiDownload />
+      </Button>
       <Heading>Your results:</Heading>
+
       <Text>
         <br />
         <b>Department:</b> {capitalize(department)}
