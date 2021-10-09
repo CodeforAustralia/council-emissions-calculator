d709c86c00435d0b12cd8d45f89a917f9a8363f1
meng <loh.yumeng@gmail.com>
2021-10-09 18:27:15 +1100

    set a theme for Heading components

--- pages/_app.js
+++ pages/_app.js
@@ -10,12 +10,29 @@ const breakpoints = createBreakpoints({
   lg: "1400px",
 })
 
+const headingStyle = {
+  baseStyle: {
+    fontWeight: 700,
+    as: "h1",
+    mt: ["24px", "97px"],
+    textAlign: "center"
+  },
+  sizes: {
+    xl: {
+      fontSize: [26, 36]
+    }
+  }
+}
+
 const theme = extendTheme({
   fonts: {
     heading: "Public Sans",
     body: "Public Sans",
   },
   breakpoints,
+  components: {
+    Heading: headingStyle
+  }
 });
 
 function MyApp({ Component, pageProps }) {
