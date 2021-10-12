8b2d17a522f9c25b4da7497b142e97a494d0daf1
meng <loh.yumeng@gmail.com>
2021-10-12 23:19:15 +1100

    suppress heeded lint warning

--- pages/form/Question2.jsx
+++ pages/form/Question2.jsx
@@ -30,6 +30,7 @@ export default function Question2() {
     if (parseInt(answers.nWorkDays) < answers.travelDays.length) {
       setDaysSelected([]);
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const saveAnswers = () => {
