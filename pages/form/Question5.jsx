5b6d509cc1d99b6490765729a0bd8a5a301dde6e
Colette Liu <colette.liu@gmail.com>
2021-10-14 22:20:13 +1100

    add Text component for Q5 and Flex

--- pages/form/Question5.jsx
+++ pages/form/Question5.jsx
@@ -1,48 +1,57 @@
 import { useState } from "react";
-import { Box, Text, Select } from "@chakra-ui/react";
+import { Heading, Box, Text, Select, Flex } from "@chakra-ui/react";
 import Layout from "../../components/Layout/Layout";
 import useForm from "../../components/FormProvider";
 import { departments } from "../../utils/constants";
 import {
   BackButton,
-  ContinueButton, 
+  ContinueButton,
 } from "../../components/LinkButton/LinkButton";
 import capitalize from "../../utils/capitalize";
 import Q5Progress from "../../public/images/progress-bar/q5-progress-dots.svg";
+import Q5Cloud from "../../public/images/clouds/cloud-q5.svg";
 import { sendFormResponse } from "../../utils/dbApi";
 
 export default function Question5() {
   const { answers, setAnswers } = useForm();
   const [department, setDepartment] = useState(answers.department);
-
   console.log(`answers so far: ${JSON.stringify(answers)}`);
 
-  const saveAnswers = () => setAnswers((prev) => ({ ...prev, department }));
+  const saveAnswers = () =>
+    setAnswers((prev) => ({ ...prev, department: selectedMode }));
   return (
     <Layout isText={true} Progress={Q5Progress}>
-
       <Box pos="absolute" top={["2", "5"]} left={["2", "10"]}>
         <BackButton href="/form/Question5" onClick={saveAnswers} />
       </Box>
+      <Q5Cloud />
 
-      <Text textAlign="center">
-        Which department do you work for?
-      </Text>
-      <Select
-        mt={8}
-        maxW={280}
-        placeholder="Please select"
-        onChange={(e) => setDepartment(e.target.value)}
-      >
-        {departments.map((department) => (
-          <option key={department} value={department}>
-            {capitalize(department)}
-          </option>
-        ))}
-      </Select>
+      <Heading>Which department do you work for?</Heading>
+      <Flex flexDirection={["column", "row"]}>
+        <Text
+          mt="4"
+          textAlign={"center"}
+          maxWidth={"450px"}
+          width={["100%", "100%"]}
+        >
+          This helps us to provide more transparency to support the outcome of
+          this initiative.
+        </Text>
+        <Select
+          mt={8}
+          maxW={280}
+          placeholder="Please select"
+          onChange={(e) => setDepartment(e.target.value)}
+        >
+          {departments.map((department) => (
+            <option key={department} value={department}>
+              {capitalize(department)}
+            </option>
+          ))}
+        </Select>
 
-      <ContinueButton href="/form/Question6" onClick={saveAnswers} />
-
+        <ContinueButton href="/form/Question6" onClick={saveAnswers} />
+      </Flex>
     </Layout>
   );
 }
