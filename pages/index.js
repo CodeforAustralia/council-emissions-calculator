0c6215a3f875e8fc720cb55921efb565fd83a1a7
meng <loh.yumeng@gmail.com>
2021-10-15 16:02:31 +1100

    implement welcome page

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,63 @@
-import { Heading, Text } from "@chakra-ui/react";
+import {
+  Heading,
+  Text,
+  Image,
+  Box,
+  Flex,
+} from "@chakra-ui/react";
 import Layout from "../components/Layout/Layout";
 import LinkButton from "../components/LinkButton/LinkButton";
-import HomePageImage from "../public/images/home-page-image.svg";
+import Q1Cloud from "../public/images/clouds/cloud-q1.svg";
+
+function Animation() {
+  return (
+    <Box
+      backgroundImage="url('/images/drive-animation/green-rect.svg')"
+      backgroundPosition="bottom"
+      backgroundRepeat="no-repeat"
+      minW={["200px", "421px"]}
+      maxH="421px"
+      overflow="hidden"
+    >
+      <Box
+      w={["300px", "421px"]}
+      h={["250px", "421px"]}
+        backgroundImage="url('/images/drive-animation/drive-animation-cut.gif')"
+        backgroundSize="cover"
+        backgroundPosition="bottom"
+      />
+      {/* <Image w={["300px", "421px"]} src="/images/drive-animation/drive-animation-cut.gif" alt="animated graphic" /> */}
+      <Box pos="relative" bottom="390px" left="220px" display={["none", "block"]}>
+        <Q1Cloud />
+      </Box>
+    </Box>
+  );
+}
+
+const spacing = 6;
 
 export default function Home() {
   return (
-    <Layout isText={true} >
-      <Heading>Your Weekly Commute</Heading>
-      <HomePageImage />
-      <Text>
-        <b>Let&apos;s calculate the impact of your work commute.</b>
+    <Layout isText={true} contentMaxWidth="container.lg" >
+      <Flex alignItems="center" flexDir={["column", "row"]}>
+        <Animation />
+        <Box p={[5, 10]}>
+          <Heading textAlign={["center", "start"]}>
+            Help calculate Council&apos;s carbon emission
+          </Heading>
+          <Text mt={spacing}>
+            We&apos;ll ask some questions about how you usually travel to and from work, and your suggestions on how council could help you to travel differently.
+          </Text>
+          <Text mt={spacing}>
+            We&apos;ll compile your response into a report, which will be shared by your work representative once the survey is complete.
+          </Text>
+          <Text mt={spacing}>
+            This survey will take approximately <b>3 minutes</b> to complete.
+          </Text>
+          <LinkButton href="/form/Question1" width={["90vw", "173px"]}>Start</LinkButton>
+        </Box>
+      </Flex>
 
-        We&apos;ll ask some questions about how you got to and from your
-        work last week. This doesn&apos;t include any travel you may have done
-        as part of your work.
-      </Text>
-      <Text>
-        This form will take approximately 3 minutes to complete.
-      </Text>
-      <LinkButton href="/form/Question1">Start</LinkButton>
     </Layout>
   );
 }
