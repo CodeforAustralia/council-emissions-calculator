daec116e3faacae16886612984b22504731e3d86
hqtan <hanqing.tan@gmail.com>
2021-10-24 16:52:35 +1100

    add more `consol.log`s

--- pages/api/logger.js
+++ pages/api/logger.js
@@ -39,6 +39,7 @@ const logger = async (req, res) => {
   switch (req.method) {
     case 'POST':
       console.log(`[INFO] ${req.method} sending logs...`);
+      console.log(`[INFO] payload: ${req.body} `);
       await pinologger.info(data, JSON.stringify(req.body));
       res.status(200).send(req.body);
       break;
