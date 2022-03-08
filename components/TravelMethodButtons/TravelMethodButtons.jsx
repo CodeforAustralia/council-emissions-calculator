5f413308935d1464f84c0f85275595dd1064c109
Amutha Muhunthan <tamamutha@gmail.com>
2022-03-09 09:53:15 +1100

    changed name of the function to travelMethodButoonsContainer

--- components/TravelMethodButtons/TravelMethodButtons.jsx
+++ components/TravelMethodButtons/TravelMethodButtons.jsx
@@ -1,6 +1,6 @@
-import { Box, Button, Text, SimpleGrid, Icon, Flex } from '@chakra-ui/react';
+import { Box, Button, Text, SimpleGrid, Icon, Flex } from "@chakra-ui/react";
 
-export function TravelMethodButtons({
+export function TravelMethodButtonsContainer({
   transportIcon,
   modesOfTransport,
   handleTransportMode,
@@ -11,32 +11,31 @@ export function TravelMethodButtons({
       columns={3}
       defaultValue={transportMode}
       id="selector"
-      width={['305px', '548px']}
-      mr={'auto'}
-      ml={'auto'}
+      width={["305px", "548px"]}
+      mr={"auto"}
+      ml={"auto"}
     >
       {modesOfTransport.map((mode, i) => (
-        <Box height="80px" textAlign={'center'} key={mode}>
+        <Box height="80px" textAlign={"center"} key={mode}>
           {/* buttons */}
 
           <Button
             variant="outline"
             fontSize={[13, 15]}
             color="#044B7F"
-            height={['58px', '57px']}
-            width={['91.67px', '141px']}
+            height={["58px", "57px"]}
+            width={["91.67px", "141px"]}
             border="1px"
-            // borderColor="044B7F.300"
             onChange={handleTransportMode}
             colorScheme="#044B7F"
             value={mode}
           >
             <Box
               pos="absolute"
-              top={['3px', '2px']}
-              pb={['4px', '3px']}
-              width={['30px', '32px']}
-              height={['30px', '32px']}
+              top={["3px", "2px"]}
+              pb={["4px", "3px"]}
+              width={["30px", "32px"]}
+              height={["30px", "32px"]}
               mb="12px"
             >
               <Icon as={transportIcon[i]} />
