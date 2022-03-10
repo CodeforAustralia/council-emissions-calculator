805ac1b314ddc1366d06a7ec41def1e02079478b
Amutha Muhunthan <tamamutha@gmail.com>
2022-03-11 09:47:22 +1100

    removed comment lines

--- components/TravelMethodButtons/TravelMethodButtons.jsx
+++ components/TravelMethodButtons/TravelMethodButtons.jsx
@@ -22,27 +22,6 @@ export function TravelMethodButtons({ handleTransPortMode }) {
               mode={mode}
               ind={i}
             />
-            {/* <Button
-              fontSize={["16px", "16px"]}
-              color="#044B7F"
-              height={["100px", "80px"]}
-              width={["91.67px", "150px"]}
-              border="1px"
-              onChange={handleTransportMode}
-              colorScheme="#044B7F"
-              value={mode}
-            >
-              <Box
-                pos="absolute"
-                width={["30px", "32px"]}
-                height={["30px", "32px"]}
-                fontSize={"16px"}
-                mb={"22px"}
-              >
-                <Icon as={transportIcon[i]} />
-              </Box>
-              {mode}
-            </Button> */}
           </Box>
         ))}
       </SimpleGrid>
