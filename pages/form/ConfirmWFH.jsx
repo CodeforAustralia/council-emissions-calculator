754230ed6b64cc0c137b4bc5d574932b8cd7a322
Olga <88268603+OlaShabalina@users.noreply.github.com>
2022-03-26 21:17:32 +1100

    create a ConfirmPage to place a collapsible container component

---
+++ pages/form/ConfirmWFH.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import { 
+  Box,
+  Heading,
+  Flex
+} from "@chakra-ui/react";
+import Layout from "../../components/Layout/Layout";
+import useForm from "../../components/FormProvider";
+import Q1Progress from "../../public/images/progress-bar/q1-progress-dots.svg";
+import Q3Cloud from "../../public/images/clouds/cloud-q3.svg";
+import LinkButton, { BackButton } from "../../components/LinkButton/LinkButton";
+import { useRouter } from "next/router";
+import { sendLogs } from "../../utils/sendLogs";
+
+export default function ConfirmWFH() {
+
+  // code from previoud version below, no changes made
+
+  const { answers, setAnswers } = useForm();
+
+  const saveAnswers = () => {
+    // saving radio button selection
+    setAnswers(prev => ({ ...prev }));
+  }
+
+  const router = useRouter();
+
+  const logMessage = (msg) => {
+    let incentiveMsg = () => {
+      if (!!answers.incentive) {return "<filled>"}
+      else return "<empty>"
+    }
+    return {
+      page: router.pathname,
+      event: msg,
+      ...answers,
+      incentive: incentiveMsg(),
+    }
+  }
+
+  // function to save data and show logs on save
+  const saveDataAndShowLog = (logMsg) => {
+    saveAnswers();
+    sendLogs(logMessage(logMsg));
+  };
+
+  return (
+    <Layout 
+      isText={true} 
+      Progress={Q1Progress}
+    >
+      <Box 
+        pos="absolute" 
+        top={["2", "5"]} 
+        left={["2", "10"]}
+      >
+        <BackButton
+          href="/"
+          onClick={() => {
+            saveDataAndShowLog("Back button clicked");
+          }}
+        />
+      </Box>
+      <Box>
+        <Flex
+          justify="center"
+          direction="column"
+        >
+          <Q3Cloud />
+          <Heading 
+            mt={10} 
+            mb={10}
+          >
+            Please confirm the details below
+          </Heading>
+        </Flex>
+        <Flex
+          justify="end"
+        >
+          <LinkButton
+            href={"/form/Distance"}
+            width="105px"
+            topMargin="0"
+            H="55px"
+            justifySelf="right"
+            onClick={() => saveDataAndShowLog("Next button clicked")}
+          >
+            Next
+          </LinkButton>
+        </Flex>
+      </Box>
+    </Layout>
+  );
+}
