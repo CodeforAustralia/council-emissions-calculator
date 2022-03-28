597bc315a34f62a02a787028f1c20168d5e1eec7
Olga <88268603+OlaShabalina@users.noreply.github.com>
2022-03-28 19:34:08 +1100

    set up initial layout and functionality for the component

--- components/ConfirmDetailsContainer/ConfirmDetailsContainer.jsx
+++ components/ConfirmDetailsContainer/ConfirmDetailsContainer.jsx
@@ -1,31 +1,53 @@
 import { useState } from "react";
-import { Text, Box, SimpleGrid, GridItem, Flex, Collapse, Spacer } from "@chakra-ui/react";
+import { Text, Box, Flex, Collapse, Spacer, Icon } from "@chakra-ui/react";
 import LinkButton from "../LinkButton/LinkButton";
+import Pencil from "../../public/images/other/Pencil.svg"
 
-export default function ConfirmDetailsContainer() {
+export default function ConfirmDetailsContainer({ methodIcon, title }) {
+
+  const [ openContainer, setOpenContainer ] = useState(false);
+
+  const handleClick = () => {
+    (openContainer == false) ? setOpenContainer(true) : setOpenContainer(false)
+  }
 
   return (
     <Box 
-      justify="center" 
+      direction="column"
+      align="space-between" 
       borderWidth="2px" 
       borderRadius="lg" 
-      minW="750px" 
-      py="8%"
+      px="3%"
+      py="5%"
     >
     <Flex>
-      <Flex
-        justifyContent="space-between"
-      >
-        <div>Main Icon</div>
-        <div>Title & Text</div>
-        <div>Edit Icon</div>
+      <Flex>
+        <Icon 
+          justifySelf="end"
+          w={7} 
+          h={7} 
+          as={methodIcon}
+        />
+        <Spacer />
+        <Text
+        >
+          {title}
+        </Text>
       </Flex>
-      <Collapse>
+      <Spacer />
+        <Icon 
+          justifySelf="end"
+          as={Pencil} 
+          w={7} 
+          h={7} 
+          onClick={() => handleClick()}
+        />
+      </Flex>
+      <Collapse in={openContainer}>
         <Text>
           Test data inside collapsible part of the container
         </Text>
       </Collapse>
-    </Flex>
     </Box>
   )
 }
