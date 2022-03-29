c07220b74c0759aa07e9818ec9ea5fa58e0a4c8a
hqtan <hanqing.tan@gmail.com>
2022-03-30 09:02:09 +1100

    add state vars for `daysOfTheWeek`, `wfhDays` and `onsiteDays`

--- components/FormProvider/FormProvider.jsx
+++ components/FormProvider/FormProvider.jsx
@@ -2,10 +2,62 @@ import { useState, createContext } from "react";
 
 export const FormContext = createContext();
 
+// initial state for days of the week has info if it's selected or not (instead of having 2 separate states)
+const daysOfTheWeek = [
+  {
+    id: 0,
+    day: "Monday",
+    isSelected: false,
+    isDisable: false
+  },
+  {
+    id: 1,
+    day: "Tuesday",
+    isSelected: false,
+    isDisable: false
+  },
+  {
+    id: 2,
+    day: "Wednesday",
+    isSelected: false,
+    isDisable: false
+  },
+  {
+    id: 3,
+    day: "Thursday",
+    isSelected: false,
+    isDisable: false
+  },
+  {
+    id: 4,
+    day: "Friday",
+    isSelected: false,
+    isDisable: false
+  },
+  {
+    id: 5,
+    day: "Saturday",
+    isSelected: false,
+    isDisable: false
+  },
+  {
+    id: 6,
+    day: "Sunday",
+    isSelected: false,
+    isDisable: false
+  }
+];
+
 // Example set of answers
 // {
 //     km: 12, // value is an integer
 //     numDaysWorked: 5, // value is an integer from 1-7
+//     wfhDays: ["Wednesday", "Friday", "Saturday"]
+//          // value is a list of length 0-7 containing days of week,
+//          // where respondent worked from home
+//     onsiteDays: ["Monday", "Tuesday"]
+//          // value is a list of length 0-7 containing days of week,
+//          // where respondent worked onsite
 //     travelDays: ["Monday", "Wednesday", "Friday"],
 //          // value is a list of length 0-7 containing days of week
 //     mainTransportMode: "bus",
@@ -19,6 +71,8 @@ export const FormContext = createContext();
 const initialAnswers = {
   km: "",
   numDaysWorked: 0,
+  wfhDays: [],
+  onsiteDays: [],
   travelDays: [],
   workMode: "",
   mainTransportMode: "",
