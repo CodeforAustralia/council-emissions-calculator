91342fbf0aca230171800b93136d601bb87328af
Matt McKenzie <matthew.mitchell.mckenzie@gmail.com>
2022-04-25 11:27:33 +1000

    first looks at this

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -98,7 +98,7 @@ export function Content({ children, background, maxContainerWidth }) {
         centerContent
         maxW={maxContainerWidth || "container.sm"}
         px={5}
-        py={["65px", "97px"]}
+        py={["20px", "20px"]}
       >
         {children}
       </Container>
