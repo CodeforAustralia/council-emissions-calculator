17cf6187855e1242c6f33e4b3dbf84e99e9f700b
hqtan <hqtan@users.noreply.github.com>
2022-05-20 12:41:42 +1000

    initial work on results page

--- pages/results.js
+++ pages/results.js
@@ -5,13 +5,7 @@ import Layout from "../components/Layout/Layout";
 import capitalize from "../utils/capitalize";
 import { FiDownload } from "react-icons/fi";
 import { getTripTotalsTop3 } from "./api/trips";
-import Highcharts from "highcharts";
-import HighchartsExporting from "highcharts/modules/exporting";
-import HighchartsReact from "highcharts-react-official";
 
-if (typeof Highcharts === "object") {
-  HighchartsExporting(Highcharts);
-}
 
 export async function getServerSideProps() {
   const tripjson = await getTripTotalsTop3();
@@ -26,38 +20,6 @@ export async function getServerSideProps() {
   };
 }
 
-const TripsChart = ({ data }) => {
-  // console.log(`data: ${ JSON.stringify(data) }`);
-
-  if (!data) return <div>I got nothing...</div>;
-
-  let topTransportModes = data.map((t) => t[0]);
-  let tripCounts = data.map((t) => Number(t[1]));
-  // console.log(`topTransportModes: ${ JSON.stringify(topTransportModes) }`);
-  // console.log(`tripCounts: ${ JSON.stringify(tripCounts) }`);
-
-  const hichartsOpts = {
-    title: {
-      text: "Trip Count by Transport Mode",
-    },
-    chart: {
-      type: "bar",
-    },
-    xAxis: {
-      // categories: ['Walking', 'Train/tram', 'E-scooter']
-      categories: topTransportModes,
-    },
-    series: [
-      {
-        name: "Trip Count",
-        data: tripCounts,
-        // data: [37, 30, 28]
-      },
-    ],
-  };
-
-  return <HighchartsReact highcharts={Highcharts} options={hichartsOpts} />;
-};
 
 export default function Results({ data }) {
   const { answers } = useForm();
@@ -65,25 +27,10 @@ export default function Results({ data }) {
 
   return (
     <Layout isText={true}>
-      {/* Button for download */}
-      <Button className="resultbtn" colorScheme="blue">
-        Download as PDF or CSV {""}
-        <FiDownload />
-      </Button>
-
-      <Heading>Your results:</Heading>
-      <TripsChart data={data} />
-
-      <Text>
-        <br />
-        <b>Department:</b> {capitalize(department)}
-        <br />
-        <b>Anything that would change your mind:</b> {incentive}
-        <br />
-        <br />
-        <b>Daily travel:</b> {km}km
-        <br />
-      </Text>
+      <div style={"border: 2px solid red"}>
+        Intro section...
+      <div/>
+          
     </Layout>
   );
 }
