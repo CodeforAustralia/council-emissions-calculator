97eef5deefcbb53b332e9b6f843f31beda431d46
Olga <88268603+OlaShabalina@users.noreply.github.com>
2022-05-22 12:47:03 +1000

    comment out the data for now to work on the design first

--- pages/results.js
+++ pages/results.js
@@ -2,23 +2,23 @@ import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
 import React from "react";
 import useForm from "../components/FormProvider";
 import Layout from "../components/Layout/Layout";
-import capitalize from "../utils/capitalize";
-import { FiDownload } from "react-icons/fi";
-import { getTripTotalsTop3 } from "./api/trips";
+// import capitalize from "../utils/capitalize";
+// import { FiDownload } from "react-icons/fi";
+// import { getTripTotalsTop3 } from "./api/trips";
 
 
-export async function getServerSideProps() {
-  const tripjson = await getTripTotalsTop3();
+// export async function getServerSideProps() {
+//   const tripjson = await getTripTotalsTop3();
 
-  // console.log(`is tripjson ready?: ${ !!tripjson }`);
-  // console.log(`tripjson: ${ JSON.stringify(tripjson) }`);
+//   // console.log(`is tripjson ready?: ${ !!tripjson }`);
+//   // console.log(`tripjson: ${ JSON.stringify(tripjson) }`);
 
-  return {
-    props: {
-      data: tripjson,
-    },
-  };
-}
+//   return {
+//     props: {
+//       data: tripjson,
+//     },
+//   };
+// }
 
 
 export default function Results({ data }) {
