87b09f871c758b8f57adda20fdaeb49c025329fc
Olga <88268603+OlaShabalina@users.noreply.github.com>
2022-05-25 17:40:44 +1000

    create download results component skeleton

---
+++ components/ResultsPageComponents/DownloadResults/DownloadResults.jsx
@@ -0,0 +1,17 @@
+import {
+  Box,
+
+} from "@chakra-ui/react";
+
+export default function DownloadResults() {
+
+  return (
+    <Box
+      direction="column"
+      align={["center", "space-between"]}
+      borderWidth="2px"
+    >
+      Test
+    </Box>
+  );
+}
