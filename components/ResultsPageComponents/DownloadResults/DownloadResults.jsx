e57b65c3b6ef42c8b15fb17869ac917f78829b27
Olga <88268603+OlaShabalina@users.noreply.github.com>
2022-05-25 20:16:25 +1000

    style download survey data part

--- components/ResultsPageComponents/DownloadResults/DownloadResults.jsx
+++ components/ResultsPageComponents/DownloadResults/DownloadResults.jsx
@@ -1,5 +1,5 @@
 import {
-  Text, Flex, Button,
+  Text, Flex, Button, Box
 } from "@chakra-ui/react";
 import DownloadPdf from "../../../public/images/download-results-icons/DownloadPdf.svg"
 import DownloadCsv from "../../../public/images/download-results-icons/DownloadCsv.svg"
@@ -31,16 +31,27 @@ export default function DownloadResults() {
           <Box><DownloadPdf /></Box>
           <Text fontSize="12px" font="Public Sans" fontWeight={600} lineHeight="13px">Download this page</Text>
         </Flex>
-        <Flex>
+        <Flex 
+          direction="column" 
+          background="rgba(221, 221, 229, 0.1)" 
+          border="0.613005px solid #DDDDE5"
+          boxShadow="0px 0px 22.5px rgba(35, 47, 78, 0.14)"
+          borderRadius="0px 13.5px 0px 0px"
+          align="center"
+          justify="center"
+          width="173px"
+          height="86px"
+          px="30%"
+        >
           <DownloadCsv />
-          <Text>Downaload survey data</Text>
+          <Text fontSize="12px" font="Public Sans" fontWeight={600} lineHeight="13px">Downaload survey data</Text>
         </Flex>
       </Flex>
       <Flex>
         <Lamp />
       </Flex>
       <Flex>
-        <Flex>
+        <Flex direction="column">
           <Text>How is it calculated?</Text>
           <Text>Developed in compliance with Greenhouse Gas Protocol.</Text>
         </Flex>
