8f5383f7780aa7b68e92f1b3593fc5ff6d1cf08a
Olga <88268603+OlaShabalina@users.noreply.github.com>
2022-05-27 19:36:11 +1000

    complete flex alignment and structure pf the component

--- components/ResultsPageComponents/DownloadResults/DownloadResults.jsx
+++ components/ResultsPageComponents/DownloadResults/DownloadResults.jsx
@@ -10,12 +10,13 @@ export default function DownloadResults() {
 
   return (
     <Flex
-      minwidth="364px"
+      minwidth="350px"
       direction="column"
       align="center"
       justify="center"
+      gap="17px"
     >
-      <Flex gap="17px" justify="center">
+      <Flex gap="17px" justify="center" height="86px">
         <Flex 
           direction="column" 
           background="rgba(221, 221, 229, 0.1)" 
@@ -24,12 +25,10 @@ export default function DownloadResults() {
           borderRadius="13.5px 0px 0px 0px"
           align="center"
           justify="center"
-          width="173px"
-          height="86px"
-          px="30%"
+          width="180px"
         >
           <Box><DownloadPdf /></Box>
-          <Text fontSize="12px" font="Public Sans" fontWeight={600} lineHeight="13px">Download this page</Text>
+          <Text textAlign="center" fontSize="12px" font="Public Sans" fontWeight={600} px="35%" lineHeight="13px">Download this page</Text>
         </Flex>
         <Flex 
           direction="column" 
@@ -39,30 +38,40 @@ export default function DownloadResults() {
           borderRadius="0px 13.5px 0px 0px"
           align="center"
           justify="center"
-          width="173px"
-          height="86px"
-          px="30%"
+          width="180px"
         >
           <DownloadCsv />
-          <Text fontSize="12px" font="Public Sans" fontWeight={600} lineHeight="13px">Downaload survey data</Text>
+          <Text fontSize="12px" font="Public Sans" fontWeight={600} textAlign="center" px="29%" lineHeight="13px">Downaload survey data</Text>
         </Flex>
       </Flex>
-      <Flex>
-        <Lamp />
-      </Flex>
-      <Flex>
-        <Flex direction="column">
-          <Text>How is it calculated?</Text>
-          <Text>Developed in compliance with Greenhouse Gas Protocol.</Text>
+      <Flex 
+        direction="column" 
+        border="0.613005px solid #DDDDE5"
+        boxShadow="0px 0px 7.2px rgba(35, 47, 78, 0.25)"
+        borderRadius="0px 0px 13.5px 13.5px"
+        align="center"
+        justify="center"
+        width="377px"
+        gap="17px"
+        >
+        <Flex height="65px" background="#E5F4E3" width="100%" justify="center" align="center">
+          <Lamp />
         </Flex>
         <Flex>
-          <GHGProtocol />
+          <Flex direction="column">
+            <Text>How is it calculated?</Text>
+            <Text>Developed in compliance with Greenhouse Gas Protocol.</Text>
+          </Flex>
+          <Flex>
+            <GHGProtocol />
+          </Flex>
+        </Flex>
+        <Flex>
+          <Text>Read how we have calculated and reported your emissions:</Text>
+          <Button>Read</Button>
         </Flex>
       </Flex>
-      <Flex>
-        <Text>Read how we have calculated and reported your emissions:</Text>
-        <Button>Read</Button>
-      </Flex>
+      
     </Flex>
   );
 }
