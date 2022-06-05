a108f79805b261b101dd24be8d26f0a440b56999
Olga <88268603+OlaShabalina@users.noreply.github.com>
2022-06-05 19:49:43 +1000

    design top left part

--- components/ResultsPageComponents/SurveyInfo/SurveyInfo.jsx
+++ components/ResultsPageComponents/SurveyInfo/SurveyInfo.jsx
@@ -2,8 +2,17 @@ import { Text, Flex, Button, Box } from "@chakra-ui/react";
 
 export default function SurveyInfo() {
   return (
-    <Flex>
-      Test
+      <Flex>
+        <Flex flex={1} justify="center">
+          <Flex height="90%">
+            <Calendar />
+          </Flex>
+          <Flex direction="column" justify="space-around" paddingX="13px">
+            <Text fontSize={["26px","49px"]} color="#03385F" lineHeight={1}>12 - 19</Text>
+            <Text fontSize="26px" color="#03385F" lineHeight={1}>April</Text>
+          </Flex>
+        </Flex>
+      </Flex>
     </Flex>
   );
 }
