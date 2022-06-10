ee6dfd110a9c254fe0c97e4c8e1e95a225cd5d8e
Olga <88268603+OlaShabalina@users.noreply.github.com>
2022-06-10 22:12:45 +1000

    create a skeleton of the component and slign on the page

---
+++ components/ResultsPageComponents/SurveyIntro/SurveyIntro.jsx
@@ -0,0 +1,14 @@
+import { Text, Flex, Button, Box } from "@chakra-ui/react";
+
+export default function SurveyIntro() {
+  return (
+    <Flex
+      minWidth="350px"
+      alignSelf={["center", "start"]}
+      flex={[1, 2]}
+      m="50px"
+    >
+      Test
+    </Flex>
+  );
+}
