eb196eb4c84802ca64a08b77b2d6a1c93a21e7b0
Olga <88268603+OlaShabalina@users.noreply.github.com>
2022-06-10 22:15:44 +1000

    import icons to the page

--- components/ResultsPageComponents/SurveyIntro/SurveyIntro.jsx
+++ components/ResultsPageComponents/SurveyIntro/SurveyIntro.jsx
@@ -1,4 +1,9 @@
 import { Text, Flex, Button, Box } from "@chakra-ui/react";
+import WalkingMan from "../../../public/images/survey-intro-icons/walking-man.svg";
+import Car from "../../../public/images/survey-intro-icons/car.svg";
+import Carpool from "../../../public/images/survey-intro-icons/carpool.svg";
+import Motorcycle from "../../../public/images/survey-intro-icons/motorcycle.svg";
+import Train from "../../../public/images/survey-intro-icons/Train.svg";
 
 export default function SurveyIntro() {
   return (
@@ -9,6 +14,11 @@ export default function SurveyIntro() {
       m="50px"
     >
       Test
+      <WalkingMan />
+      <Car />
+      <Carpool />
+      <Motorcycle />
+      <Train />
     </Flex>
   );
 }
