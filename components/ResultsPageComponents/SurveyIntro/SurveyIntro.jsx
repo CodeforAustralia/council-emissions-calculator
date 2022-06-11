8fa3ac0204d3e7bc1819a4290388167c0887c667
Olga <88268603+OlaShabalina@users.noreply.github.com>
2022-06-11 16:29:14 +1000

    style iconts and text for travel methods

--- components/ResultsPageComponents/SurveyIntro/SurveyIntro.jsx
+++ components/ResultsPageComponents/SurveyIntro/SurveyIntro.jsx
@@ -9,46 +9,65 @@ export default function SurveyIntro() {
   return (
     <Flex
       minWidth="350px"
+      maxWidth="1100px"
       alignSelf={["center", "start"]}
       flex={[1, 2]}
-      m="50px"
       direction="column"
-      gap={["20px", "50px"]}
+      gap={["10px", "20px"]}
     >
       <Flex direction="column">
-        <Text fontWeight={400} fontSize="50px">Work Commute Survey Results</Text>
-        <Text fontWeight={400} fontSize="20px">The results page provides the outcome from our recent survey. We have recently embarked on our 2026 challenge. We aim to increase the use of Active, Public and Shared commute methods to 20% or more. Currently, we are on our way to meeting the 2026 challenge. With our collective effort, we can improve our impact on the environment.</Text>
+        <Text fontSize="50px">Work Commute Survey Results</Text>
+        <Text fontSize="20px">The results page provides the outcome from our recent survey. We have recently embarked on our 2026 challenge. We aim to increase the use of Active, Public and Shared commute methods to 20% or more. Currently, we are on our way to meeting the 2026 challenge. With our collective effort, we can improve our impact on the environment.</Text>
       </Flex>
       <Flex direction="column">
         <Text fontWeight={600} fontSize="27px">Things to note before reading the results</Text>
-        <Text fontWeight={400} fontSize="19px">Travel methods have been grouped into Active-Public-Shared methods and Individual methods: </Text>
+        <Text fontSize="19px">Travel methods have been grouped into <b>Active-Public-Shared</b> methods and <b>Individual</b> methods: </Text>
       </Flex>
-      <Flex>
-        <Flex direction="column">
-            <Flex direction="column">
-              <Text>Active/Public/Shared Methods:</Text>
-              <Text>Lower carbon emission per kilometre travel per person</Text>
+      <Flex direction="column" gap="10px">
+        <Flex gap="30px" direction={["column", "row"]} justify={["center", "space-between"]} align="center">
+          <Flex direction="column">
+            <Text fontWeight={600} color="#022640" fontSize="22px">Active/Public/Shared Methods:</Text>
+            <Text color="#03385F">Lower carbon emission per kilometre travel per person</Text>
+          </Flex>
+          <Flex gap="30px">
+            <Flex direction="column" width="100px" align="center">
+              <Flex justify="center" align="center" width="95px" height="95px" background="#E6EEF3" boxShadow="0px 1px 30px rgba(230, 238, 243, 0.99)" borderRadius="50%">
+                <WalkingMan />
+              </Flex>
+              <Text textAlign="center" width="80px" color="#044B7F">Walk/Run/Cycle</Text>
             </Flex>
-            <Flex direction="column">
-              <Text>Individual Methods:</Text>
-              <Text>Higher carbon emission per kilometre travel per person.</Text>
-            </Flex>
+            <Flex direction="column" width="100px" align="center">
+              <Flex justify="center" align="center" width="95px" height="95px" background="#E6EEF3" boxShadow="0px 1px 30px rgba(230, 238, 243, 0.99)" borderRadius="50%">
+                <Train />
+              </Flex>  
+              <Text color="#044B7F">Bus/Train</Text>
+            </Flex>  
+            <Flex direction="column" width="100px" align="center">
+              <Flex justify="center" align="center" width="95px" height="95px" background="#E6EEF3" boxShadow="0px 1px 30px rgba(230, 238, 243, 0.99)" borderRadius="50%">
+                <Carpool />
+              </Flex>
+              <Text color="#044B7F">Taxi/Carpool</Text>
+            </Flex>     
+          </Flex>
         </Flex>
-        <Flex flexWrap="wrap">
-          <Flex>
-            <WalkingMan />
+        <Flex gap="30px" direction={["column", "row"]} justify={["center", "space-between"]} align="center">
+          <Flex direction="column">
+            <Text fontWeight={600} color="#022640" fontSize="22px">Individual Methods:</Text>
+            <Text color="#03385F">Higher carbon emission per kilometre travel per person.</Text>
           </Flex>
-          <Flex>
-            <Car />
-          </Flex>
-          <Flex>
-            <Carpool />
-          </Flex>
-          <Flex>
-            <Motorcycle />
-          </Flex>
-          <Flex>
-            <Train />
+          <Flex gap="30px">
+            <Flex direction="column" width="100px" align="center">
+              <Flex justify="center" align="center" width="95px" height="95px" background="rgba(214, 158, 46, 0.36)" boxShadow="0px 1px 30px rgba(214, 158, 46, 0.25)" borderRadius="50%">
+                <Car />
+              </Flex>
+              <Text color="#044B7F">Car</Text>
+            </Flex>
+            <Flex direction="column" width="100px" align="center">
+              <Flex justify="center" align="center" width="95px" height="95px" background="rgba(214, 158, 46, 0.36)" boxShadow="0px 1px 30px rgba(214, 158, 46, 0.25)" borderRadius="50%">
+                <Motorcycle />
+              </Flex>
+              <Text color="#044B7F">Motorbike</Text>
+            </Flex>
           </Flex>
         </Flex>
       </Flex>
