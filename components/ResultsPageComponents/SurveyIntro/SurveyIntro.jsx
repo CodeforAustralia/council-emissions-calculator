7110b129da71fb5eebfe6f693b3dd71fc7d63fd0
Olga <88268603+OlaShabalina@users.noreply.github.com>
2022-06-11 13:31:23 +1000

    add content to the skeleton

--- components/ResultsPageComponents/SurveyIntro/SurveyIntro.jsx
+++ components/ResultsPageComponents/SurveyIntro/SurveyIntro.jsx
@@ -12,28 +12,44 @@ export default function SurveyIntro() {
       alignSelf={["center", "start"]}
       flex={[1, 2]}
       m="50px"
+      direction="column"
+      gap={["20px", "50px"]}
     >
-      <Text></Text>
-      <Text></Text>
-      <Text></Text>
-      <Text></Text>
+      <Flex direction="column">
+        <Text fontWeight={400} fontSize="50px">Work Commute Survey Results</Text>
+        <Text fontWeight={400} fontSize="20px">The results page provides the outcome from our recent survey. We have recently embarked on our 2026 challenge. We aim to increase the use of Active, Public and Shared commute methods to 20% or more. Currently, we are on our way to meeting the 2026 challenge. With our collective effort, we can improve our impact on the environment.</Text>
+      </Flex>
+      <Flex direction="column">
+        <Text fontWeight={600} fontSize="27px">Things to note before reading the results</Text>
+        <Text fontWeight={400} fontSize="19px">Travel methods have been grouped into Active-Public-Shared methods and Individual methods: </Text>
+      </Flex>
       <Flex>
-        <Flex firection="column">
-            <Flex>
-              <Text></Text>
-              <Text></Text>
+        <Flex direction="column">
+            <Flex direction="column">
+              <Text>Active/Public/Shared Methods:</Text>
+              <Text>Lower carbon emission per kilometre travel per person</Text>
             </Flex>
-            <Flex>
-              <Text></Text>
-              <Text></Text>
+            <Flex direction="column">
+              <Text>Individual Methods:</Text>
+              <Text>Higher carbon emission per kilometre travel per person.</Text>
             </Flex>
         </Flex>
-        <Flex>
-          <WalkingMan />
-          <Car />
-          <Carpool />
-          <Motorcycle />
-          <Train />
+        <Flex flexWrap="wrap">
+          <Flex>
+            <WalkingMan />
+          </Flex>
+          <Flex>
+            <Car />
+          </Flex>
+          <Flex>
+            <Carpool />
+          </Flex>
+          <Flex>
+            <Motorcycle />
+          </Flex>
+          <Flex>
+            <Train />
+          </Flex>
         </Flex>
       </Flex>
     </Flex>
