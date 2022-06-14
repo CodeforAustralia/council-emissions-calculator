e2a1126f1912ae56b835d6417342cb81af615b54
Olga <88268603+OlaShabalina@users.noreply.github.com>
2022-06-14 22:13:02 +1000

    style one walking man mini-component

--- components/ResultsPageComponents/WorkArrangement/WorkArrangement.jsx
+++ components/ResultsPageComponents/WorkArrangement/WorkArrangement.jsx
@@ -20,6 +20,18 @@ export default function WorkArrangement() {
         <Text fontSize="19px">
           The infographics below illustrate responders’ working arrangement across the week. This information helps identify the percentage of staff commuting to work in regular (on-site) and intermittent (hybrid) patterns.
         </Text>
+          <Flex width="30%" direction="column" py="20px">
+            <Flex justify="flex-end">
+              <Line maxWidth="150px" />
+              <Flex direction="column">
+                <Text fontWeight={500} fontSize="36px" lineHeight="23px" color="#D69E2E">42%</Text>
+                <Text fontWeight={200} fontSize="24px" color="#044B7F">Hybrid</Text>
+              </Flex>
+            </Flex>
+            <Flex justify="center" maxHeight="280px" height="100%" borderBottom="2.12px solid #D69E2E" pb="15px">
+              <Traveler />
+            </Flex>
+          </Flex>
       </Flex>
     </Flex>
   );
