7cac951ce4823896f712881c1b0291d16c3467a8
Olga <88268603+OlaShabalina@users.noreply.github.com>
2022-06-14 22:13:38 +1000

    style the rest of the page using the same mini-component

--- components/ResultsPageComponents/WorkArrangement/WorkArrangement.jsx
+++ components/ResultsPageComponents/WorkArrangement/WorkArrangement.jsx
@@ -1,4 +1,6 @@
 import { Text, Flex } from "@chakra-ui/react";
+import Traveler from "../../../public/images/work-arrangement-results-icons/traveler.svg";
+import Line from "../../../public/images/work-arrangement-results-icons/l-shape-line.svg";
 
 export default function WorkArrangement() {
   return (
@@ -11,15 +13,17 @@ export default function WorkArrangement() {
       direction="column"
       gap={["10px", "20px"]}
       px={["5px", "50px"]}
+      py={["25px", "50px"]}
       justify="center"
     >
       <Flex direction="column">
-        <Text fontWeight={600} fontSize="33px" lineHeight="74px">
+        <Text fontWeight={600} fontSize="33px" lineHeight="37px" py="15px">
           Things to note before reading the results
         </Text>
-        <Text fontSize="19px">
+        <Text fontSize="19px" py="15px">
           The infographics below illustrate responders’ working arrangement across the week. This information helps identify the percentage of staff commuting to work in regular (on-site) and intermittent (hybrid) patterns.
         </Text>
+        <Flex direction={["column", "row"]} align={["center", "flex-end"]}>
           <Flex width="30%" direction="column" py="20px">
             <Flex justify="flex-end">
               <Line maxWidth="150px" />
@@ -32,6 +36,31 @@ export default function WorkArrangement() {
               <Traveler />
             </Flex>
           </Flex>
+          <Flex width="30%" direction="column" py="20px">
+            <Flex justify="flex-end">
+              <Line maxWidth="150px" />
+              <Flex direction="column">
+                <Text fontWeight={500} fontSize="36px" lineHeight="23px" color="#D69E2E">32%</Text>
+                <Text fontWeight={100} fontSize="24px" color="#044B7F">On-site</Text>
+              </Flex>
+            </Flex>
+            <Flex justify="center" maxHeight="200px" height="75%" borderBottom="2.12px solid #D69E2E" pb="15px">
+              <Traveler />
+            </Flex>
+          </Flex>
+          <Flex width="30%" direction="column" py="20px">
+            <Flex justify="flex-end">
+              <Line maxWidth="150px" />
+              <Flex direction="column">
+                <Text fontWeight={500} fontSize="36px" lineHeight="23px" color="#D69E2E">26%</Text>
+                <Text fontWeight={100} fontSize="24px" color="#044B7F">WFH</Text>
+              </Flex>
+            </Flex>
+            <Flex justify="center" maxHeight="160px" height="50%" borderBottom="2.12px solid #D69E2E" pb="15px">
+              <Traveler />
+            </Flex>
+          </Flex>
+        </Flex>
       </Flex>
     </Flex>
   );
