33a160441f45d920d9db901a511c7245dcd06f12
Olga <88268603+OlaShabalina@users.noreply.github.com>
2022-06-15 21:52:08 +1000

    create a new component for trip count

---
+++ components/ResultsPageComponents/TripCountAndTravelMethods/TripCountAndTravelMethods.jsx
@@ -0,0 +1,19 @@
+import { Text, Flex } from "@chakra-ui/react";
+
+export default function TripCountAndTravelMethods() {
+  return (
+    <Flex
+      minWidth="350px"
+      maxWidth="1100px"
+      alignSelf={["center", "start"]}
+      align={["center", "flex-start"]}
+      flex={[1, 2]}
+      direction="column"
+      gap={["10px", "20px"]}
+      px={["5px", "50px"]}
+      py={["25px", "50px"]}
+      justify="center"
+    >
+    </Flex>
+  );
+}
