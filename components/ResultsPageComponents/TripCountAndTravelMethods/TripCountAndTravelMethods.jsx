2543650582834d5d3988ebd38e8b4a4128c9adb9
Olga <88268603+OlaShabalina@users.noreply.github.com>
2022-06-25 14:41:40 +1000

    add trip count chart to the TripCount component

--- components/ResultsPageComponents/TripCountAndTravelMethods/TripCountAndTravelMethods.jsx
+++ components/ResultsPageComponents/TripCountAndTravelMethods/TripCountAndTravelMethods.jsx
@@ -1,4 +1,5 @@
 import { Text, Flex } from "@chakra-ui/react";
+import TripCount from "./PieChart";
 
 export default function TripCountAndTravelMethods() {
   return (
@@ -22,8 +23,8 @@ export default function TripCountAndTravelMethods() {
           The three donut graphs below help us to understand how we commute to work. The graph on the left ‘Trip Count’ determines the total trip counts and the breakdown by individual and Active-Public-shared travel methods. The two graphs on the right Active-Public-shared travel and Individual travel show us the total trips for each travel method and their breakdown by mode.  
         </Text>
         <Flex direction={["column", "row"]} align="center">
-          <Flex width="50%">
-            Large chart
+          <Flex>
+            <TripCount />
           </Flex>
           <Flex direction="column" width="50%">
             <Flex>Small top chart</Flex>
