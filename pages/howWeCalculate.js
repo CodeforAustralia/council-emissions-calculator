13d14790d69c71be80e1e088ecdea2df4fc3a422
Olga <88268603+OlaShabalina@users.noreply.github.com>
2022-07-17 19:01:49 +1000

    create a table of content block

--- pages/howWeCalculate.js
+++ pages/howWeCalculate.js
@@ -1,4 +1,4 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, UnorderedList, ListItem, Link } from "@chakra-ui/react";
 import Layout from "../components/Layout/Layout";
 import TextBlock from "../components/TextBlock/TextBlock"
 
@@ -6,16 +6,37 @@ export default function howWeCalculate() {
 
   return (
     <Layout isText={true} maxContainerWidth="100%">
-      <Flex direction={["column", "row"]} px={["10px", "50px"]} py="48px">
+      <Flex direction={["column", "row"]} px={["10px", "50px"]} py="48px" gap="50px">
         <Flex width={["100%", "70%"]} direction="column">
-          <Text fontWeight={600} fontSize="34px" lineHeight="78px">Carbon Emission Calculation and Reporting</Text>
+          <Text fontWeight={600} fontSize="34px">Carbon Emission Calculation and Reporting</Text>
           <TextBlock title="How does it work?">
             The carbon calculator uses commuting information supplied by Council staff to calculate the volume of carbon emissions associated with staff commuting to and from work.
             The results report displays valuable insights on total carbon emissions by mode of transport, daily trends in emissions, and a breakdown of emissions by the department. These insights can help inform Council strategies and plans to promote more sustainable modes of transport and reduce commuting emissions.
           </TextBlock>
         </Flex>
-        <Flex width={["100%", "30%"]} direction="column">
-          Right
+        <Flex width={["100%", "30%"]} direction="column" shadow="0px 0px 7.2px rgba(35, 47, 78, 0.25)" borderRadius="15px">
+          <Flex background="#E5F4E3">
+            <Text fontWeight={700} fontSize="24px" lineHeight="29px" p="34px">Table of Contents</Text>
+          </Flex>
+          <Flex>
+            <UnorderedList p="24px 34px">
+              <ListItem fontWeight={700} fontSize="16px" lineHeight="24px" color="#044B7F" pb="12px">
+                <Link>What is the Caculator and Result page?</Link>
+              </ListItem>
+              <ListItem fontWeight={700} fontSize="16px" lineHeight="24px" color="#044B7F" pb="12px">
+                <Link>How does it work?</Link>
+              </ListItem>
+              <ListItem fontWeight={700} fontSize="16px" lineHeight="24px" color="#044B7F" pb="12px">
+                <Link>What can you achieve with the calculator?</Link>
+              </ListItem>
+              <ListItem fontWeight={700} fontSize="16px" lineHeight="24px" color="#044B7F" pb="12px">
+                <Link>Who made this calculator?</Link>
+              </ListItem>
+              <ListItem fontWeight={700} fontSize="16px" lineHeight="24px" color="#044B7F">
+                <Link>Why did we make this calculator?</Link>
+              </ListItem>
+            </UnorderedList>
+          </Flex>
         </Flex>
       </Flex>
     </Layout>
