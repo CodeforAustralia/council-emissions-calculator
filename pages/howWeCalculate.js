fd576957a552017de2c842b27b48baa2da7e6ecc
Olga <88268603+OlaShabalina@users.noreply.github.com>
2022-07-17 19:07:44 +1000

    change padding for the title

--- pages/howWeCalculate.js
+++ pages/howWeCalculate.js
@@ -8,7 +8,7 @@ export default function howWeCalculate() {
     <Layout isText={true} maxContainerWidth="100%">
       <Flex direction={["column", "row"]} px={["10px", "50px"]} py="48px" gap="50px">
         <Flex width={["100%", "70%"]} direction="column">
-          <Text fontWeight={600} fontSize="34px">Carbon Emission Calculation and Reporting</Text>
+          <Text fontWeight={600} fontSize="34px" pb={[0,"30px"]} textShadow="0px 4px 4px rgba(0, 0, 0, 0.25)">Carbon Emission Calculation and Reporting</Text>
           <TextBlock title="How does it work?">
             The carbon calculator uses commuting information supplied by Council staff to calculate the volume of carbon emissions associated with staff commuting to and from work.
             The results report displays valuable insights on total carbon emissions by mode of transport, daily trends in emissions, and a breakdown of emissions by the department. These insights can help inform Council strategies and plans to promote more sustainable modes of transport and reduce commuting emissions.
