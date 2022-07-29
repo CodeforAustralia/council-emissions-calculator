9a617dffee1888f8104204fd89e8928190b4614e
Olga <88268603+OlaShabalina@users.noreply.github.com>
2022-07-29 13:50:20 +1000

    add and style the rest of the text blocks

--- pages/howWeCalculate.js
+++ pages/howWeCalculate.js
@@ -21,15 +21,34 @@ export default function howWeCalculate() {
           >
             Carbon Emission Calculation and Reporting
           </Text>
+          <TextBlock title="What is this?">
+            The carbon calculator uses commuting information supplied by Council staff to calculate the volume of carbon emissions associated with staff commuting to and from work.
+            The results display valuable insights on commuting habits, preferred transport types, and emissions generated. These insights can help inform Council's 2026 goal to increase the use of Active, Public, and shared commute methods to 20% or more.
+          </TextBlock>
           <TextBlock title="How does it work?">
-            The carbon calculator uses commuting information supplied by Council
-            staff to calculate the volume of carbon emissions associated with
-            staff commuting to and from work. The results report displays
-            valuable insights on total carbon emissions by mode of transport,
-            daily trends in emissions, and a breakdown of emissions by the
-            department. These insights can help inform Council strategies and
-            plans to promote more sustainable modes of transport and reduce
-            commuting emissions.
+            The calculator uses inputs from the staff survey, taking into account commute distance and transport modes.
+            It plugs those numbers into this formula from the <Link color="#044B7F" textDecoration="underline" textUnderlineOffset="3px" fontWeight={700}>Greenhouse Gas Protocol</Link> that calculates the CO2e emissions from employee travel.
+            The results report provides details on the calculations and emissions factors used.
+          </TextBlock>
+          <Flex direction="column" width="90%" py="24px" gap="10px">
+            <Flex justify="center" background="#60AAA0">
+              <Text fontSize="16px" color="#FFFFFF" fontWeight={700} p="10px">CO2e emissions from employee travel = </Text>
+            </Flex>
+            <Flex background="#F3F3F3" justify="center">
+              <CalculationFormula />
+            </Flex>
+            <Flex>
+              <Text fontSize="13px" fontStyle="italic">{"Reference: Calculation formula [7.1] Distance-based method, Technical Guidance for Calculating Scope 3 Emissions, accessed 8th July 2022<https://ghgprotocol.org/sites/default/files/standards_supporting/Chapter7.pdf>"}</Text>
+            </Flex>
+          </Flex>
+          <TextBlock title="What can you achieve with the Calculator?">
+            The results from the carbon calculator provide a point-in-time snapshot of a Council’s staff commute emissions. Analysing trends and patterns relating to transport modes and daily emissions will allow Council to devise strategies such as ridesharing, incentivised modes of transport, or flexible work arrangements, to name just a few.
+          </TextBlock>
+          <TextBlock title="Who made this calculator?">
+            While the formula was developed by the Greenhouse Gas Protocol Centre of Excellence, the survey and results page were built by a volunteer group of problem solvers, <Link color="#044B7F" textDecoration="underline" textUnderlineOffset="3px" fontWeight={700} >Civic Makers</Link>, trying to solve the question of “How can we make our communities better?” using civic-tech. We can’t solve every problem related to our community at once, so we decided to build tools that support Council sustainability initiatives. Civic Makers is run by <Link color="#044B7F" textDecoration="underline" textUnderlineOffset="3px" fontWeight={700} >Code for Australia</Link>, a not-for-profit organisation that builds tech stuff that matters for all levels of government.
+          </TextBlock>
+          <TextBlock title="Why did we make this calculator?">
+            Due to climate change, we want to use our technical skills for the public good by supporting governments in responding to the climate crisis. We believe assisting them with this calculator would be an excellent place to direct our efforts while council budgets and resources are under pressure. 
           </TextBlock>
         </Flex>
         <Flex
