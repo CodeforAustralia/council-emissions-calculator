83234b8e37492f63af65ef3e1285d7108f48a758
Amutha Muhunthan <tamamutha@gmail.com>
2022-08-08 15:53:19 +1000

    add alignment for the csv button

--- components/ResultsPageComponents/DownloadResults/DownLoadCsv.jsx
+++ components/ResultsPageComponents/DownloadResults/DownLoadCsv.jsx
@@ -2,25 +2,60 @@ import { Button, Box, Text } from "@chakra-ui/react";
 import DownloadCsv from "../../../public/images/download-results-icons/DownloadCSV.svg";
 import { CSVLink } from "react-csv";
 
+// export default function DownLoadcsv() {
+//   return (
+//     <Box
+//       style={{
+//         backgroundColor: "white",
+//         _hover: { bg: "#ebedf0" },
+//       }}
+//     >
+//       <Button
+//         style={{
+//           width: "177px",
+//           height: "56px",
+//           border: "none",
+//           outline: "none",
+//           backgroundColor: "white",
+//           _hover: {
+//             bg: "#ebedf0",
+//           },
+//         }}
+//       >
+//         <CSVLink data="../../public/pdfcsv/Sheet1test.csv">
+//           {" "}
+//           <DownloadCsv />
+//         </CSVLink>
+//       </Button>
+//       <Text
+//         fontSize="12px"
+//         fontFamily="Public Sans"
+//         fontWeight={600}
+//         textAlign="center"
+//         px="29%"
+//         lineHeight="13px"
+//         color="#044B7F"
+//       >
+//         Downaload survey data
+//       </Text>
+//     </Box>
+//   );
+// }
 export default function DownLoadcsv() {
   return (
-    <Box
-      style={{
-        backgroundColor: "white",
-        _hover: { bg: "#ebedf0" },
-      }}
-    >
+    <Box>
       <Button
-        style={{
-          width: "177px",
-          height: "56px",
-          border: "none",
-          outline: "none",
-          backgroundColor: "white",
-          _hover: { bg: "#ebedf0" },
+        width="177px"
+        height="56px"
+        border="none"
+        outline="none"
+        backgroundColor="white"
+        _hover={{
+          bg: "#ebedf0",
+          borderRadius: "0px 0px 0px 0px",
         }}
       >
-        <CSVLink data="../../public/pdfcsv/Sheet1test.csv">
+        <CSVLink data="../../public/filePdfCsv/Sheet1test.csv">
           {" "}
           <DownloadCsv />
         </CSVLink>
