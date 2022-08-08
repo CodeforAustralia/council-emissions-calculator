b7b845f0fdd0cc26c1612f7a8b758b14fde4361d
Ricky <rickyliew@hotmail.com>
2022-08-08 15:07:37 +1000

    remote commented code

--- pages/_app.js
+++ pages/_app.js
@@ -1,41 +1,9 @@
 import "../styles/globals.css";
 import "@fontsource/public-sans/400.css";
 import "@fontsource/public-sans/700.css";
-// import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { FormProvider } from "../components/FormProvider";
-// import { createBreakpoints } from "@chakra-ui/theme-tools";
 import theme from "../styles/theme";
-
-/*
-const breakpoints = createBreakpoints({
-  sm: "630px",
-  lg: "1400px",
-});
-const headingStyle = {
-  baseStyle: {
-    fontWeight: 700,
-    as: "h1",
-    mt: ["24px", "97px"],
-    textAlign: "center",
-  },
-  sizes: {
-    xl: {
-      fontSize: [26, 36],
-    },
-  },
-};
-const theme = extendTheme({
-  fonts: {
-    heading: "Public Sans",
-    body: "Public Sans",
-  },
-  breakpoints,
-  components: {
-    Heading: headingStyle,
-  },
-});
-*/
 
 function MyApp({ Component, pageProps }) {
   console.log("theme: ", theme);
