89d3916d0cd4ed47ede81919e2692de7dfd9c9ff
Ricky <rickyliew@hotmail.com>
2022-08-11 10:57:39 +1000

    remove guide lines

--- components/ResultsPageComponents/SharedComponents/ResultContentSection.jsx
+++ components/ResultsPageComponents/SharedComponents/ResultContentSection.jsx
@@ -2,10 +2,8 @@ import { Box } from "@chakra-ui/react";
 
 export default function ResultContentSection({ isShaded, children }) {
   return (
-    <Box width="100%" my={5} px="68px" border="1px dashed blue">
-      <Box bg={isShaded ? "#FAFAFA" : "inherit"} border="1px dashed red">
-        {children}
-      </Box>
+    <Box width="100%" my={5} px="68px">
+      <Box bg={isShaded ? "#FAFAFA" : "inherit"}>{children}</Box>
     </Box>
   );
 }
