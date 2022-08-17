df2af6d4b17b201534bea61162b2cff910a08635
Ricky <rickyliew@hotmail.com>
2022-08-17 14:32:57 +1000

    edit layouts

--- components/ResultsPageComponents/SharedComponents/ResultContentSection.jsx
+++ components/ResultsPageComponents/SharedComponents/ResultContentSection.jsx
@@ -2,8 +2,10 @@ import { Box } from "@chakra-ui/react";
 
 export default function ResultContentSection({ isShaded, children }) {
   return (
-    <Box width="100%" my={5} px="68px">
-      <Box bg={isShaded ? "#FAFAFA" : "inherit"}>{children}</Box>
+    <Box width="100%" my={5} px="68px" border="1px dashed darkblue">
+      <Box bg={isShaded ? "#FAFAFA" : "inherit"} border="1px dotted blue">
+        {children}
+      </Box>
     </Box>
   );
 }
