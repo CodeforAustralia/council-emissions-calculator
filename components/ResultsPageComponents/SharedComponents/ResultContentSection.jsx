8d87853d4b139d5d3a3fb4c8f3454ddf7cf31d7e
Ricky <rickyliew@hotmail.com>
2022-08-17 15:40:46 +1000

    fix layout for the overview page to align with other sections

--- components/ResultsPageComponents/SharedComponents/ResultContentSection.jsx
+++ components/ResultsPageComponents/SharedComponents/ResultContentSection.jsx
@@ -2,10 +2,8 @@ import { Box } from "@chakra-ui/react";
 
 export default function ResultContentSection({ isShaded, children }) {
   return (
-    <Box width="100%" my={5} px="68px" border="1px dashed darkblue">
-      <Box bg={isShaded ? "#FAFAFA" : "inherit"} border="1px dotted blue">
-        {children}
-      </Box>
+    <Box width="100%" my={5} px="68px">
+      <Box bg={isShaded ? "#FAFAFA" : "inherit"}>{children}</Box>
     </Box>
   );
 }
