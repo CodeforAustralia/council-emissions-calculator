d1bf4e9c98ffe9d93cf0733d418888a8f80725fe
hqtan <hanqing.tan@gmail.com>
2023-02-19 01:02:16 +1100

    is216: align left margin of `CommuteDays` section with  `WorkArrangement`

--- pages/results.js
+++ pages/results.js
@@ -97,7 +97,15 @@ export default function Results({ data }) {
         >
           <WorkArrangement workMode={data["work-mode"]} />
         </Flex>
-        <CommuteDays data={data} />
+        <Flex
+          width="100%"
+          gap={["40px", "90px"]}
+          wrap="wrap"
+          justify={["center", "left"]}
+          align={["center", "flex-start"]}
+        >
+          <CommuteDays data={data} />
+        </Flex>
       </ResultContentSection>
       <ResultContentSection isShaded={true}>
         <CommuteDistanceDistribution data={data} />
