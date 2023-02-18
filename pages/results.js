b81fc66a2b5990a02f3c7f6e44eeea709dfeb132
hqtan <hanqing.tan@gmail.com>
2023-02-18 17:55:03 +1100

    is216: align left margin of `TopThree` section with `SurveyOverview`

--- pages/results.js
+++ pages/results.js
@@ -63,12 +63,20 @@ export default function Results({ data }) {
         </Flex>
       </ResultContentSection>
       <ResultContentSection>
-        <TopThree
-          topThree={data["TopThreeData"]}
-          totalDistance={data["total-distance"]}
-          totalTripCount={data["total-trip-count"]}
-          totalEmissions={data["total-co2-emissions-tonnes"]}
-        />
+        <Flex
+          width="100%"
+          gap={["40px", "90px"]}
+          wrap="wrap"
+          justify={["center", "left"]}
+          align={["center", "flex-start"]}
+        >
+          <TopThree
+            topThree={data["TopThreeData"]}
+            totalDistance={data["total-distance"]}
+            totalTripCount={data["total-trip-count"]}
+            totalEmissions={data["total-co2-emissions-tonnes"]}
+          />
+        </Flex>
 
         <TripCountAndTravelMethods dataAboutTrips={data["dataAboutTrips"]} />
       </ResultContentSection>
